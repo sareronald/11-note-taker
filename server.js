e49b6f3ca9b04f47bb4593dfb44ab57d21bb6c77
Sarah Ronald <sareronald@hotmail.com>
2020-10-31 21:28:49 +1100

    notes can be added to the express app

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const dayjs = require("dayjs");
+
+// Read the notes DB file - this data source holds an array of information
+let notesDb = JSON.parse(fs.readFileSync("./db/db.json"));
+console.log(notesDb);
 
 // Express configuration
 const app = express();
@@ -15,7 +20,7 @@ app.use(express.static("public"));
 
 // ROUTES
 // =======================================================
-// index.html is the default file
+// index.html is the default file so no need to app.get
 
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
@@ -23,21 +28,47 @@ app.get("/notes", function (req, res) {
 
 // API GET request
 app.get("/api/notes", function (req, res) {
-  return res.json("db.json");
+  // do I need to return this? ReferenceError notesBD is not defined
+  res.json(notesDb);
 });
 
-// ADD, SAVE, DELETE from DATABASE
+// ADD & SAVE from DATABASE
 // =======================================================
-// app.post("/api/notes", function (req, res){
-// const newNote = req.body;
-// what is req.body? Body of the request - expecting to have some parameters and these sit in the body
-// req = request
+app.post("/api/notes", function (req, res) {
+  let newNote = req.body;
+  console.log(newNote);
+  // generate unique id using the date and time
+  // https://day.js.org/docs/en/display/format
+  let dateTimeNow = dayjs().format("DDMMYYYYTHHmmss");
+  console.log(dateTimeNow);
+  newNote.id = dateTimeNow;
+
+  notesDb.push(newNote);
+
+  fs.writeFileSync(
+    path.resolve("./db/db.json"),
+    JSON.stringify(notesDb),
+    (err) => {
+      if (err) throw err;
+      console.log("Adding new note...");
+    }
+  );
+  res.send(newNote);
+});
+
+// DELETE from DATABASE
+// =======================================================
+// app.delete("/api/notes/:id", function (req, res) {
+//   console.log(req.params);
+//   let id = req.params.id;
+//   console.log(id);
+//   //  can not use newNote in line below...
+//   const deleteNote = notesDb.find((deleteNote) => newNote.id === id);
+//   if (deleteNote) {
+//     return res.json(deleteNote);
+//   }
+//   return res.send("No ID found");
 // });
-// $.post("/api/characters", newCharacter)
-// .then(function(data) {
-// console.log("add.html", data);
-// alert("Adding character...");
-//   });
 
 // LISTENER
 // =======================================================
