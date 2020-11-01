787f3f2db094c4bf047745df0b383794ca2b3a53
Sarah Ronald <sareronald@hotmail.com>
2020-11-02 09:30:25 +1100

    deleting notes from Db

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const path = require("path");
 const fs = require("fs");
 const dayjs = require("dayjs");
 
-// Read the notes DB file - this data source holds an array of information
-let notesDb = JSON.parse(fs.readFileSync("./db/db.json"));
-console.log(notesDb);
-
 // Express configuration
 const app = express();
 // Sets an initial port.
@@ -16,25 +12,30 @@ const PORT = process.env.PORT || 8080;
 // Set up the Express App to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Configure Express to serve static files
 app.use(express.static("public"));
 
 // ROUTES
 // =======================================================
 // index.html is the default file so no need to app.get
 
-app.get("/notes", function (req, res) {
+app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // API GET request
-app.get("/api/notes", function (req, res) {
+app.get("/api/notes", (req, res) => {
   // do I need to return this? ReferenceError notesBD is not defined
   res.json(notesDb);
 });
 
 // ADD & SAVE from DATABASE
 // =======================================================
-app.post("/api/notes", function (req, res) {
+// Read the notes DB file - this data source holds an array of information
+let notesDb = JSON.parse(fs.readFileSync("./db/db.json"));
+console.log(notesDb);
+
+app.post("/api/notes", (req, res) => {
   let newNote = req.body;
   console.log(newNote);
   // generate unique id using the date and time
@@ -56,35 +57,33 @@ app.post("/api/notes", function (req, res) {
   res.send(newNote);
 });
 
-// DELETE from DATABASE
+// DELETE from db.json
 // =======================================================
 app.delete("/api/notes/:id", function (req, res) {
   console.log(req.params);
   const chosenID = req.params.id;
   console.log(chosenID);
-  const deleteNote = notesDb.find((id) => id.id === chosenID);
-  if (deleteNote) {
-    // what is my index here - it is not id, it is not notesDB,
-    // it is deleting any note, not the one that equals the correct id
-    // this if condition is not working... it is always true?
-    // should this be find of findIndex
-    notesDb.splice(deleteNote, 1);
-
-    fs.writeFileSync(
-      path.resolve("./db/db.json"),
-      JSON.stringify(notesDb),
-      (err) => {
-        if (err) throw err;
-        console.log("Deleting note...");
-      }
-    );
-    return notesDb;
-  }
-  console.log(notesDb);
-
-  return res.send("No ID found");
+  // Read all notes
+  const newDB = notesDb.filter((savedNote) => savedNote.id != chosenID);
+  // your formula would be to return each value into the new array if it does not match the id of the record to be deleted
+  // notesDb.splice(savedNote, 1);
+  // Writes remaining notes to db.json
+  fs.writeFileSync(
+    path.resolve("./db/db.json"),
+    JSON.stringify(newDB),
+    (err) => {
+      if (err) throw err;
+      console.log(`Deleting note ${chosenID}...`);
+    }
+  );
+  res.send(newDB);
+  // return newDB;
 });
 
+// Default Route
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "public/index.html"));
+// });
 // LISTENER
 // =======================================================
 // Starts the server
